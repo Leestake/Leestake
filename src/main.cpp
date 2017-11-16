b5707b4826efa76372b641ab0d53269ead350657
5mil <5mil@users.noreply.github.com>
2017-11-15 22:23:21 -0500

    Update main.cpp
    MerkleRoot

--- src/main.cpp
+++ src/main.cpp
@@ -3302,7 +3302,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0x83f2677edd9f7542d4c67f91a43139b55f243e76c9402fcd37eff6bc87822cb2"));
         block.print();
         assert(hash == hashGenesisBlock);
         // LEESoin: check genesis block
